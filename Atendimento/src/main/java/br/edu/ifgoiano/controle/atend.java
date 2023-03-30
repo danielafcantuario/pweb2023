01f8794b80676a4eede39b0e5f93ebd677a34237
2021108102540138 <2021108102540138@lab01-27.ifgoiano.local>
2023-03-30 16:05:11 -0300

    atendimento

---
+++ Atendimento/src/main/java/br/edu/ifgoiano/controle/atend.java
@@ -0,0 +1,33 @@
+package br.edu.ifgoiano.controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//req pega o index 
+//resp envia para o html entre a chave "soma"
+
+@WebServlet("/soma")
+public class atend extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String n1 = req.getParameter("numero1");
+		String n2 = req.getParameter("numero2");
+		
+	float soma = Float.parseFloat(n1) +  Float.parseFloat(n2);
+	System.out.println(soma);
+	
+	String htmlSoma = new String();
+	htmlSoma = "<html><body><h1> A soma é "+ soma+" <h1/><body/> <html/>";
+	
+	PrintWriter objeto = resp.getWriter();
+	objeto.println(htmlSoma);
+	
+	}
+	
+}
+
