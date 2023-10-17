41747f21dfdd3983c36bac3299d7c6beac878460
2021108102540138 <2021108102540138@lab02-05.ifgoiano.local>
2023-10-17 10:55:21 -0300

    api 2 comitando medico controller

---
+++ api/src/main/java/br/edu/ifgoiano/api/controller/MedicoController.java
@@ -0,0 +1,17 @@
+package br.edu.ifgoiano.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medico")
+public class MedicoController {
+	
+	@PostMapping
+	public void cadastrar(String medico) {
+		System.out.println(medico);
+	}
+
+	
+}
---
+++ api/src/main/resources/static/medico.js
@@ -0,0 +1,17 @@
+{
+	"id":"null",
+	"nome":"daniela",
+	"email": "daniela@gmail",
+	"cmr":95495,
+	"especialidade":"ORTOPEDIA",
+	"endereco": {
+		"id":null,
+		"logradouro":"rua rdg1",
+		"bairro": "recanto",
+		"complemento":"quadra 3 lote 17",
+		"cep":"74487442",
+		"cidade":"Goiania",
+		"estado":"Goias",
+		
+	}
+}
